7f214a351a4d675ca458f1c6310a56bb293e79ee
WataruTakahagi <t12525wt@sfc.keio.ac.jp>
2016-02-26 13:39:56 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='MAP',
+    version='0.1.0',
+    author='Kaito Seo',
+    author_email='t15523ks@sfc.keio.ac.jp',
+    url='https://github.com/kaitoseo/MAP',
+    py_modules=['MAP'],
+    description='Metabolome Analysis Platform',
+    keywords='bioinformatics data analysis metabolome',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
